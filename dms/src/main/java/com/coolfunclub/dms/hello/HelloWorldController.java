1b8be0e1709d366c8c89c66296ad0acf910ff656
KaylaMLe <kaylale2014@gmail.com>
2023-09-29 21:28:55 -0700

    added jsoup as a dependency and added a test function

--- dms/src/main/java/com/coolfunclub/dms/hello/HelloWorldController.java
+++ dms/src/main/java/com/coolfunclub/dms/hello/HelloWorldController.java
@@ -2,6 +2,13 @@ package com.coolfunclub.dms.hello;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOError;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.jsoup.nodes.Element;
+
 @RestController
 public class HelloWorldController {
     @RequestMapping("/HelloWorld")
@@ -14,6 +21,24 @@ public class HelloWorldController {
         return "(^=◕ᴥ◕=^)";
     }
 
+    // grabs today's date from the On this day section of the Wikipedia home page
+    @RequestMapping("/kml-a4")
+    public String  soupGet() {
+        try {
+            Document doc = Jsoup.connect("https://en.wikipedia.org/wiki/Main_Page").get();
+            Element otd = doc.getElementById("mp-otd");
+            Elements p = otd.getElementsByTag("p");
+            Elements b = p.first().getElementsByTag("b");
+            Elements a = b.first().getElementsByTag("a");
+            String day = a.first().text();
+
+
+            return "Today is " + day + '.';
+        } catch (Exception e) {
+            System.out.println(e);
+            return "";
+        }
+    }
 
     @RequestMapping("/dog")
     public String dog() {
